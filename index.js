505e8fbcfa9ccfb05a1b64cae0736f3a9fbfc006
Huste6 <quannghihai1@gmail.com>
2024-07-31 22:33:15 +0700

    Đổi trạng thái công việc

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ require('dotenv').config();
 
 const database = require("./config/database")
 const routesApiVer1 = require('./api/v1/router/index.router')
+const bodyParser = require('body-parser')
 
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
+
+//parse application/json
+app.use(bodyParser.json());
 
 //API routes
 routesApiVer1(app);
