b6cbdbd91f11f6f7246e6b9fd192b487523f991b
Huste6 <quannghihai1@gmail.com>
2024-07-31 16:08:38 +0700

    Chia folder + bộ lọc theo trạng thái

---
+++ api/v1/router/index.router.js
@@ -0,0 +1,7 @@
+const TaskRoutes = require('./task.router')
+
+module.exports = (app) => {
+    const version = '/api/v1';
+
+    app.use(version + '/tasks', TaskRoutes);
+}
--- index.js
+++ index.js
@@ -2,29 +2,16 @@ const express = require('express')
 
 require('dotenv').config();
 
-const Task = require("./models/task.model")
-
 const database = require("./config/database")
+const routesApiVer1 = require('./api/v1/router/index.router')
+
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/tasks",async (req,res)=>{
-    const tasks = await Task.find({
-        deleted:false,
-    })
-    res.json(tasks);
-})
-
-app.get("/tasks/detail/:id",async (req,res)=>{
-    const id = req.params.id;
-    const tasks = await Task.findOne({
-        _id:id,
-        deleted:false
-    })
-    res.json(tasks);
-})
+//API routes
+routesApiVer1(app);
 
 app.listen(port, () => {
     console.log(`example app listen on port ${port}`);
