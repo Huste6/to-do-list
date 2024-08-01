44b80c086039dc9fc881719a3044a58b45adc871
Huste6 <quannghihai1@gmail.com>
2024-08-01 14:58:13 +0700

    lưu id của người tạo task

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -129,6 +129,7 @@ module.exports.changeMulti = async (req,res) => {
 //[POST] /api/v1/tasks/change-multi
 module.exports.create = async (req,res) => {
     try{
+        req.body.createdBy = req.user.id;
         const task = new Task(req.body);
         const data = await task.save();
         res.json({
